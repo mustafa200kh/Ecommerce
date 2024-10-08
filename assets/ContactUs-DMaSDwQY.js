552de014929651f4f52f00debe860402586561dd
mustafa200kh <mustafa200kh@users.noreply.github.com>
2024-10-08 20:50:19 +0000

    deploy: 9c6333634eebe2bce760d7c19cef68598e1a0657

---
+++ assets/ContactUs-DMaSDwQY.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-kuPgm23o.js";function n(){return t.jsx("div",{className:"min-h-screen",children:"ContactUs"})}export{n as default};
---
+++ assets/PageHeader-DojtT84v.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-kuPgm23o.js";const x=({title:t})=>e.jsx("h2",{className:"text-2xl dark:text-white pt-3 pb-3 px-3 md:px-0",children:t});export{x as P};
