d01dd6dae81f367b7dc077ed9e6cb4f7817a0b09
mustafaAsem <mostafakhashan3@gmail.com>
2025-06-23 15:40:03 +0300

    fix backend connection

--- src/services/Api/axios-global.ts
+++ src/services/Api/axios-global.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://ecommercedb-oi92.onrender.com/";
-axios.defaults.baseURL = "http://localhost:5005";
+axios.defaults.baseURL = "https://ecommercedb-oi92.onrender.com/";
+// axios.defaults.baseURL = "http://localhost:5005";
