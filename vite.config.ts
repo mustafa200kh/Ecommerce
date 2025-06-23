bed4db91aa59626a9e700bc2228694e612c81ad0
mustafaAsem <mostafakhashan3@gmail.com>
2025-06-23 16:41:18 +0300

    resolving hosting issues

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import vitePluginSvgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react(), vitePluginSvgr()],
-  base: "/Ecommerce/",
+  base: "/Ecommerce",
 });
